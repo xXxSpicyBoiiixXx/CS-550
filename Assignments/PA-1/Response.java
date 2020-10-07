5e21fdb27708f74941ecef15e364d052921ee817
Md Ali <66300613+xXxSpicyBoiiixXx@users.noreply.github.com>
2020-10-07 16:01:27 -0700

    Wrong Code Uploaded

--- Assignments/PA-1/Response.java
+++ Assignments/PA-1/Response.java
@@ -1,51 +1,38 @@
 /* FILE: Response.java
  * USEAGE: --
- * DESCRIPTION: -- 
+ * DESCRIPTION: This will output the response.
  * OPTIONS: --
  * REQUIREMENTS: -- 
  * BUGS: -- 
  * AUTHOR: xXxSpicyBoiiixXx (Md Ali)
  * ORGANIZATION: IIT
  * VERSION: 1.0
- * CREATED: 10/01/2020
+ * CREATED: 10/03/2020
  * REVISION: -- 
 */
+
 
 import java.io.Serializable;
 
 public class Response implements Serializable {
+
+	private int responseCode;
+	private Object responseData;
 	
-	private int responseCode; 
-	private String responseData;
-	private Object otherData;
-	
-	public int getResponsesCode() 
-	{
+	public int getResponseCode() {
 		return responseCode;
 	}
-	
-	public void setResponseCode(int responseCode)
-	{
+
+	public void setResponseCode(int responseCode) {
 		this.responseCode = responseCode;
 	}
-	
-	public String getResponseData() 
-	{
+
+	public Object getResponseData() {
 		return responseData;
 	}
-	
-	public void setResponseData(String responseData)
-	{
-		this.responseData = responseData; 
+
+	public void setResponseData(Object responseData) {
+		this.responseData = responseData;
 	}
-	
-	public Object getOtherData() 
-	{
-		return otherData; 
-	}
-	
-	public void setOtherData(Object otherData) 
-	{
-		this.otherData = otherData; 
-	}
+
 }
